76d495ffb597c1af34a012aba0bd655a1df0d21e
LKuenzler <laurin.kue@gmail.com>
2020-03-03 13:38:39 +0100

    First commit

---
+++ src/main/kotlin/ch/lalumash/IBNG/IbngApplication.kt
@@ -0,0 +1,11 @@
+package ch.lalumash.IBNG
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class IbngApplication
+
+fun main(args: Array<String>) {
+	runApplication<IbngApplication>(*args)
+}
---
+++ src/test/kotlin/ch/lalumash/IBNG/IbngApplicationTests.kt
@@ -0,0 +1,13 @@
+package ch.lalumash.IBNG
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class IbngApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
